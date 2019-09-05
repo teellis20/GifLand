b20096f78d1696e456deb7de9e16e0280365b4e7
Tellis <teellis20@gmail.com>
2019-09-04 23:45:48 -0700

    final push

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GifLand
+Welcome to GifLand!
+
+Using the giphy api, I've created a web page where you can search gifs of your favorite wrestlers! (or just any gifs at all)
+
+To navigate the page, either select one of the pre-populated buttons or create your own with the search bar. 
+
+Once the gifs load to the page, you can view the animation by clicking on the gif still.
--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,12 +1,63 @@
-// api ezSy7xkZNG8LrRcs8Pgpy2T5PVTc14f6
+$(function(){
+    getButtons(gifArray, "searchButton","#buttonLand");
+})
 
-var searchWord = "shaq";
-var urlQuery = "http://api.giphy.com/v1/gifs/search?q=" + searchWord + "&api_key=ezSy7xkZNG8LrRcs8Pgpy2T5PVTc14f6";
 
-$.ajax({url:urlQuery,method:'GET'})
-    .done(function(response){
-        console.log(response.data[0].embed_url);
-        $("#gifLand").html("<img scr=" + response.data[0].embed_url + ">");
-    });
+    var gifArray = ["The Rock", "Macho Man", "Hulk Hogan", "Rey Mysterio", "Booker T", "Ric Flair"];
+
+    function getButtons(gifArray, classToAdd, areaToAdd){
+        $(areaToAdd).empty();
+        for (var i = 0; i < gifArray.length; i++) {
+            var a = $("<button>");
+            a.addClass(classToAdd);
+            a.attr("data-type", gifArray[i]);
+            a.text(gifArray[i]);
+            $(areaToAdd).append(a);
+        }
+    }
 
    
+
+    $(document).on("click",".searchButton", function() {
+        $("#gifLand").empty();
+        var type = $(this).data("type");
+        console.log(type);
+        var buttonUrl = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=ezSy7xkZNG8LrRcs8Pgpy2T5PVTc14f6";
+        $.ajax({url:buttonUrl,method:'GET'})
+    .done(function(response){
+        for(var i = 0; i < response.data.length; i++) {
+            var searchDiv = $("<div class='search-item'>");
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $("<img>");
+            image.attr("src", still);
+            image.attr("data-still", still);
+            image.attr("data-animated", animated);
+            image.attr("data-state", "still");
+            image.addClass("searchImage");
+            searchDiv.append(image);
+            $("#gifLand").append(searchDiv);
+            
+        }
+    });
+    });
+
+    $(document).on("click",".searchImage", function(){
+        var state = $(this).attr("data-state");
+        if (state == "still") {
+            $(this).attr("src", $(this).data("animated"));
+            $(this).attr("data-state", "animated");
+        } else {
+            $(this).attr("src", $(this).data("still"));
+            $(this).attr("data-state", "still");
+        }
+    })
+
+    $("#submit-button").on("click", function(){
+        var newSearch = $("input").eq(0).val();
+        gifArray.push(newSearch);
+        getButtons(gifArray,"searchButton", "#buttonLand");
+        return false;
+    })
+
+    
